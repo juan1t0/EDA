a9c78c0aedeeaf144e8573de6eb15546f73414e0
juan1t0 <juanpablo.heredia@ucsp.edu.pe>
2017-09-13 11:09:47 -0500

    Add files via upload

---
+++ list.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+struct nodo{
+	T valor;
+	nodo<T> *next;
+	
+	nodo(T x, nodo<T>* nn){
+		valor =x;
+		next=nn;
+	}
+};
+template <class T>
+struct lista{
+	nodo<T>* head;
+	
+	bool buscar(T vv, nodo<T>**&){
+		nodo<T>** th = &head;
+		while(**th != 0){
+			if(**th == vv)
+				return true;
+			th=*th.
+		}
+		return false;
+	}
+};
+int main(int argc, char *argv[]) {
+	
+	return 0;
+}
+
